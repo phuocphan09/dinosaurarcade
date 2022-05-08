526b10d5cc3abfbb3f582f1e9bcfc8dbf45e2e0f
Phuoc Phan <phuocphan09@gmail.com>
2022-05-08 14:48:04 +0700

    Initial Commit

---
+++ dinosaurarcade/dinosaurarcadeApp.swift
@@ -0,0 +1,17 @@
+//
+//  dinosaurarcadeApp.swift
+//  dinosaurarcade
+//
+//  Created by LAP13335 on 08/05/2022.
+//
+
+import SwiftUI
+
+@main
+struct dinosaurarcadeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
